b8dfffd0129124d330193a67d17aba045e69dd8c
Jesse Garcia <skaterjess2012@gmail.com>
2018-09-12 21:42:22 -0600

    unfished rework of the backend

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const path = require('path');
 const jsonFile = __dirname + '/users.json';
+const uuidv1 = require('uuid/v1');
 
 const app = express();
 
@@ -13,61 +14,49 @@ app.use('/', express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/userListing', (req, res) => {
+app.get('/userListing', (req, res) => {
     fs.readFile(jsonFile, 'utf8', (err, data) => {
         if (err) console.log(err);
-
         let jsonData = JSON.parse(data);
-        let usrObj = {
-            uid: req.body.uid,
+        res.render('userListing', {users: jsonData.users});
+    });
+});
+
+app.post('/addUser', (req, res) => {
+    res.redirect('/userListing');
+    fs.readFile(jsonFile, 'utf8', (err, data) => {
+        if (err) console.log(err);
+        let jsonData = JSON.parse(data);
+        let userObj = {
+            uid: uuidv1(),
+            id: req.body.id,
             name: req.body.name,
             email: req.body.email,
-            age: req.body.age,
-            delete: null
+            age: req.body.age
         }
-
-        jsonData.users.push(usrObj);
-        res.render('userListing', {users: jsonData.users});
-
-        fs.writeFile(jsonFile, JSON.stringify(jsonData), 'utf8', err => console.log(err));
+        jsonData.users.push(userObj);
+        // res.render('userListing', {users: jsonData.users});
+        fs.writeFile(jsonFile, JSON.stringify(jsonData), 'utf8', (err, data) => {
+            if (err) console.log(err);
+        });
     });
 });
 
-app.get('/userListing', (req, res) => {
-  fs.readFile(jsonFile, 'utf8', (err, data) => {
-    if (err) console.log(err);
-
-    let jsonData = JSON.parse(data);
-    res.render('userListing', {users: jsonData.users});
-  })
+app.get('/edit/:uid', (req, res) => {
+    let uid = req.params.uid;
+    console.log(uid);
+    res.redirect('/userListing');
 });
 
-app.post('/userEditSubmit', (req, res) => {
-  fs.readFile(jsonFile, 'utf8', (err, data) => {
-    if (err) console.log(err);
-
-    let jsonData = JSON.parse(data);
-    let editedUsers = [];
-    console.log('////////////////////');
-    console.log(req.body);
-    // jsonData.users = editedUsers;
-    res.render('userListing', {users: jsonData.users});
-
-    // fs.writeFile(jsonFile, JSON.stringify(jsonData), 'utf8', err => console.log(err));
-
-  });
+app.get('/delete/:uid', (req, res) => {
+    let uid = req.params.uid;
+    console.log(uid);
+    res.redirect('/userListing');
 });
 
-app.get('/userEdit', (req, res) => {
-    fs.readFile(jsonFile, 'utf8', (err, data) => {
-        if (err) console.log(err);
-
-        let jsonData = JSON.parse(data);
-        res.render('userEdit', {users: jsonData.users});
-
-    });
-});
+//req.params.userName
+//app.get('/user/:userName)
 
 app.listen(3000, () => {
-    console.log('Listening on port 30000');
+    console.log('Listening on port 3000');
 });
