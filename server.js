a072638e194ea034c927d0533ee9e789be15945d
Jesse Garcia <jeg6187@gmail.com>
2018-09-13 14:06:14 -0600

    FINALLY FINISHED

--- server.js
+++ server.js
@@ -14,48 +14,115 @@ app.use('/', express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/userListing', (req, res) => {
+function readJson(callback) {
     fs.readFile(jsonFile, 'utf8', (err, data) => {
         if (err) console.log(err);
-        let jsonData = JSON.parse(data);
-        res.render('userListing', {users: jsonData.users});
+        let jsonDataObject = JSON.parse(data);
+        callback(jsonDataObject);
     });
+}
+
+function writeJson(jsonDataObject, callback) {
+    fs.writeFile(jsonFile, JSON.stringify(jsonDataObject), 'utf8', (err, data) => {
+        if (err) console.log(err);
+        callback();
+    });
+}
+
+function addUser(userDataObject, callback) {
+    let readCallback = (jsonDataObject) => {
+        let users = jsonDataObject.users;
+        let writeCallback = () => {
+            callback();
+        };
+        users.push(userDataObject);
+        writeJson(jsonDataObject, writeCallback);
+    };
+    readJson(readCallback);
+}
+
+function editUser(uid, callback) {
+    let readCallBack = (jsonDataObject) => {
+        let users = jsonDataObject.users;
+        users.forEach((user, index) => {
+            if (user.uid === uid) {
+                callback(index, jsonDataObject);
+            }
+        });
+    };
+    readJson(readCallBack);
+}
+
+app.get('/userListing', (req, res) => {
+    let readCallback = (jsonDataObject) => {
+        console.log('User data returned Successfully!');
+        res.render('userListing', {users: jsonDataObject.users});
+    };
+    readJson(readCallback);
 });
 
 app.post('/addUser', (req, res) => {
-    res.redirect('/userListing');
-    fs.readFile(jsonFile, 'utf8', (err, data) => {
-        if (err) console.log(err);
-        let jsonData = JSON.parse(data);
-        let userObj = {
-            uid: uuidv1(),
-            id: req.body.id,
-            name: req.body.name,
-            email: req.body.email,
-            age: req.body.age
-        }
-        jsonData.users.push(userObj);
-        // res.render('userListing', {users: jsonData.users});
-        fs.writeFile(jsonFile, JSON.stringify(jsonData), 'utf8', (err, data) => {
-            if (err) console.log(err);
-        });
-    });
+    let body = req.body;
+    let newUserObject = {
+        uid: uuidv1(),
+        id: body.id,
+        name: body.name,
+        email: body.email,
+        age: body.age
+    }
+    let addUserCallback = () => {
+        console.log(`User successfully added!`);
+        res.redirect('/userListing');
+    };
+    addUser(newUserObject, addUserCallback);
 });
 
 app.get('/edit/:uid', (req, res) => {
     let uid = req.params.uid;
-    console.log(uid);
-    res.redirect('/userListing');
+    let editUserCallback = (index, jsonDataObject) => {
+        let users = jsonDataObject.users;
+        console.log(`${users[index].name} Returned in Edit Successfully`);
+        res.render('userEdit', {user: users[index]});
+    };
+    editUser(uid, editUserCallback);
+});
+
+app.post('/edit/:uid', (req, res) => {
+    let uid = req.params.uid;
+    let body = req.body;
+    console.log(body);
+    let newUserObject = {
+        uid: uid,
+        id: body.id,
+        name: body.name,
+        email: body.email,
+        age: body.age
+    }
+    let editUserCallback = (index, jsonDataObject) => {
+        let users = jsonDataObject.users;
+        let writeCallback = () => {
+            console.log('User Successfully Updated');
+            res.redirect('/userListing');
+        };
+        users[index] = newUserObject;
+        writeJson(jsonDataObject, writeCallback);
+    };
+    editUser(uid, editUserCallback);
 });
 
 app.get('/delete/:uid', (req, res) => {
     let uid = req.params.uid;
-    console.log(uid);
-    res.redirect('/userListing');
+    let editUserCallback = (index, jsonDataObject) => {
+        let users = jsonDataObject.users;
+        let writeCallback = () => {
+            console.log('User Has been deleted!');
+            res.redirect('/userListing');
+        }
+        users.splice(index, 1);
+        writeJson(jsonDataObject, writeCallback);
+    };
+    editUser(uid, editUserCallback);
 });
-
-//req.params.userName
-//app.get('/user/:userName)
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
